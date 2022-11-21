de08abe4a723cf1ac278f855adea8f54c673842a
alexzhang1030 <1642114555@qq.com>
2022-11-21 12:01:04 +0800

    chore: add util fn

--- core/src/managers/mod.rs
+++ core/src/managers/mod.rs
@@ -1,9 +1,6 @@
-pub mod task;
-
 use crate::structs::Pattern;
 
 pub struct Task {
     base: String,
-    dynamic: bool,
     patterns: Vec<Pattern>,
 }
--- core/src/utils/path.rs
+++ core/src/utils/path.rs
@@ -1,14 +1,32 @@
 use regex::{Error, Regex};
 
-pub fn remove_duplicate_slashes(path: &String) -> Result<String, Error> {
-    let re = Regex::new(r"/{2,}");
-    match re {
+pub fn modify_by_re(
+    original: &String,
+    reg: Result<Regex, Error>,
+    replace_str: &str,
+) -> Result<String, Error> {
+    match reg {
         Ok(regex) => {
             let res = regex
-                .replace_all(path.clone().as_mut_str(), "/")
+                .replace_all(original.clone().as_mut_str(), replace_str)
                 .to_string();
             Ok(res)
         }
+        Err(err) => Err(err),
+    }
+}
+
+// from 'c/////x' to 'c/x'
+pub fn remove_duplicate_slashes(path: &String) -> Result<String, Error> {
+    let re = Regex::new(r"/{2,}");
+    modify_by_re(path, re, "/")
+}
+
+// from 'x\\xx' to 'x/x'
+pub fn normalize_path(path: &String) -> Result<String, Error> {
+    let re = Regex::new(r"\\");
+    match modify_by_re(path, re, "/") {
+        Ok(res) => remove_duplicate_slashes(&res),
         Err(err) => Err(err),
     }
 }
@@ -18,9 +36,16 @@ pub mod test {
     use super::*;
 
     #[test]
-    pub fn test() {
+    pub fn should_remove_duplicate_slashes() {
         let str = "C://test/////echo".to_string();
         let str = remove_duplicate_slashes(&str).unwrap();
         assert_eq!(str, "C:/test/echo".to_string());
+    }
+
+    #[test]
+    pub fn should_normalize_path() {
+        let str = r"x\\\\\x".to_string();
+        let str = normalize_path(&str).unwrap();
+        assert_eq!(str, "x/x".to_string());
     }
 }
