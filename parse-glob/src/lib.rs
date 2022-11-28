637a00bcf27bada2af090a0f9e51c2f39016732b
alexzhang1030 <1642114555@qq.com>
2022-11-28 09:48:55 +0800

    chore: update

--- parse-glob/src/lib.rs
+++ parse-glob/src/lib.rs
@@ -1,4 +1,30 @@
+use is_glob::is_glob;
+use lazy_static::lazy_static;
+use std::collections::HashMap;
+
+#[derive(Clone)]
 pub struct ParseResult {}
+
+lazy_static! {
+    static ref CACHE: HashMap<String, ParseResult> = {
+        let m = HashMap::new();
+        m
+    };
+}
+
+pub fn parse(pattern: &String) -> Option<ParseResult> {
+    if !is_glob(pattern) {
+        return None;
+    }
+
+    let cache = CACHE.get(pattern);
+
+    if let Some(cache) = cache {
+        return Some((*cache).clone());
+    }
+
+    return Some(ParseResult {});
+}
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
